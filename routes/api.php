85bcd2c58a838507add59480184154ad1c46fdda
Wollos2019 <wollos2@yahoo.com>
2022-02-06 13:58:01 +0100

    last commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/clients', [ClientController::class, 'index']);
 
 //Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
