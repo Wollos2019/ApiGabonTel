18d29428cdd50ce3317f82c4c68809419de3561b
pascal setup <passy841@gmail.com>
2022-02-10 18:38:41 +0100

    add holiday

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             WorkingDaySeeder::class,
             UserSeeder::class,
-            CivilitySeeder::class
+            CivilitySeeder::class,
+            CountrySeeder::class
 
         ]);
     }
