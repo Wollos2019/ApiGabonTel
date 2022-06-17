13529918318a94066b32a504d39f4336960ae0ec
LilGiress <demanougiress@gmail.com>
2022-06-17 10:40:59 +0100

    change config in databaseseed, userSeeder,permitSeeder

--- database/seeders/PermisSeeder.php
+++ database/seeders/PermisSeeder.php
@@ -17,27 +17,27 @@ class PermisSeeder extends Seeder
     public function run()
     {
         Permit::truncate();
-        DB::table('permits')->insert(
-            [   'id'=>"1",
-                'numeroPermis'=>" 14796gtd",
-                'dateAcquisition'=>"2003-05-12",
-                //'userId'=>"0",
-            ]
-        );
-        DB::table('permits')->insert(
-            [   'id'=>"2",
-                'numeroPermis'=>"455656 mutsibushi",
-                'dateAcquisition'=>"2014-05-11",
-               // 'userId'=>"1",
-            ]
-        );
-        DB::table('permits')->insert(
-            [   'id'=>"3",
-                'numeroPermis'=>"14789 corrolla",
-                'dateAcquisition'=>"2007-07-12",
-                //'userId'=>"2",
-            ]
-        );
+//        DB::table('permits')->insert(
+//            [   'id'=>"1",
+//                'numeroPermis'=>" 14796gtd",
+//                'dateAcquisition'=>"2003-05-12",
+//                'userId'=>"0",
+//            ]
+//        );
+//        DB::table('permits')->insert(
+//            [   'id'=>"2",
+//                'numeroPermis'=>"455656 mutsibushi",
+//                'dateAcquisition'=>"2014-05-11",
+//                'userId'=>"1",
+//            ]
+//        );
+//        DB::table('permits')->insert(
+//            [   'id'=>"3",
+//                'numeroPermis'=>"14789 corrolla",
+//                'dateAcquisition'=>"2007-07-12",
+//                'userId'=>"2",
+//            ]
+//        );
 //        DB::table('permits')->insert(
 //            [   'id'=>"4",
 //                'numeroPermis'=>"pajero 15478",
