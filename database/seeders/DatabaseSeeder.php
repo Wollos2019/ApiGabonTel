468ce8ec55dfdfbd7c8afa2ed6106802c2599eb3
LilGiress <demanougiress@gmail.com>
2022-06-17 09:37:43 +0100

    change config in databaseseed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
                //DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-       // DB::statement('SET session_replication_role = \'replica\';');
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        App\User::truncate();
+        DB::statement('SET session_replication_role = \'replica\';');
+       // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+       // App\User::truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $this->call([
             WorkingDaySeeder::class,
