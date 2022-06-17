7cc3db381591408e2d2eefb837e74b6fd19f49a1
LilGiress <demanougiress@gmail.com>
2022-06-17 10:50:19 +0100

    change config in databaseseed, userSeeder,permitSeeder

--- database/migrations/2022_05_03_082919_create_permits_table.php
+++ database/migrations/2022_05_03_082919_create_permits_table.php
@@ -17,7 +17,7 @@ class CreatePermitsTable extends Migration
             $table->increments('id');
             $table->string('numeroPermis')->nullable();
             $table->date('dateAcquisition')->nullable();
-            $table->integer('userId')->unsigned();
+            $table->bigInteger('userId')->unsigned();
             $table->foreign('userId')->references('id')->on('users')->onDelete('CASCADE');
 
             $table->timestamps();
